1d3ebddf9b79fccfa59487b99469c941b1e09e5c
Thom Treebus <thomtreebus@gmail.com>
2023-06-12 16:17:07 +0100

    polish UI and add guessing logic:

--- FlagGuesser/ContentView.swift
+++ FlagGuesser/ContentView.swift
@@ -8,27 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria",
-                     "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria",
+                     "Poland", "Russia", "Spain", "UK", "US"].shuffled() // array always in random order
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         
         ZStack {
-            Color.blue
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: UnitPoint.bottom)
                 .ignoresSafeArea()
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
                     Text(countries[correctAnswer])
                         .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
                 
                 ForEach(0..<3) { number in
                     Button {
-                        // flag was tapped
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
@@ -36,7 +40,25 @@ struct ContentView: View {
                 }
             }
         }
-        
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message:  {
+            Text("Your score is ???")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
