e99fbd20ee7035a4df48185e1c949872d02a8594
Thom Treebus <thomtreebus@gmail.com>
2023-06-12 15:42:40 +0100

    Initial Commit

---
+++ FlagGuesser/FlagGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuesserApp.swift
+//  FlagGuesser
+//
+//  Created by Thom Treebus on 12/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
